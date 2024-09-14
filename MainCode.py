e0e3be32595582cebb41321ad12139fcc90a64b8
Adriano Alberto Spinola Olivares <118141438+Adrianoxzzz@users.noreply.github.com>
2024-09-13 23:27:28 -0400

    Cleaning

--- MainCode.py
+++ MainCode.py
@@ -3,32 +3,39 @@ import serial
 import chess
 import chess.engine
 
+#Serial Comunication Initialization
 esp = serial.Serial("COM4",115200)
 time.sleep(2)
 for i in range (7):
         esp.readline().decode("utf-8")
+
+#ChessBoard and Engine Initialization
 board = chess.Board();
 print(board);
 print("\n")
 engine= chess.engine.SimpleEngine.popen_uci(r"C:\stockfish-windows-x86-64-sse41-popcnt\stockfish\stockfish-windows-x86-64-sse41-popcnt")
 limit = chess.engine.Limit(time = 2.0)
+
+#Game
 while not board.is_game_over():
-    jugada = input()
+    jugada = input()    #Player Turn
     board.push_san(jugada)
     print(board)
     print("\n")
     EngineMove = engine.play(board,limit)
-    board.push(EngineMove.move)
+    board.push(EngineMove.move) #Engine Move
     print(board)
     print("\n")
-    actualFEN = board.fen();
+    actualFEN = board.fen(); #Get Game FEN
     print(actualFEN)
-    print("\n")
-    esp.write(actualFEN.encode().strip())
+    esp.write(actualFEN.encode().strip())   #Send FEN Via Serial Port
     time.sleep(1)
     print("Fen Enviado")
-    print("\n")
     response = esp.readline().decode('utf-8')
     print(response)
-    print('\n')
+    if(response.strip() != actualFEN.strip()):
+          print("FEN correctly recived")
+          print("\n")
+    else:
+          print("Error reciving FEN")
 engine.quit()
